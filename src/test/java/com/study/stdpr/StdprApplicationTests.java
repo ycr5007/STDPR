31b019b3c7b153bdf469d2d3cd62de307d7cb801
ycr <ycr5007@gmail.com>
2024-07-29 18:53:46 +0900

    Project Default Setting

---
+++ src/test/java/com/study/stdpr/StdprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.stdpr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StdprApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
