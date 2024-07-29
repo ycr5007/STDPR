d38a27d026d034f317ec75e2afdd9764547048aa
ycr <ycr5007@gmail.com>
2024-07-29 23:31:29 +0900

    Project Config Test

--- src/main/java/com/study/stdpr/StdprApplication.java
+++ src/main/java/com/study/stdpr/StdprApplication.java
@@ -2,8 +2,10 @@ package com.study.stdpr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+// exclude = SecurityAutoConfiguration.class : Spring Security Login 자동연결 제거
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class StdprApplication {
 
     public static void main(String[] args) {
